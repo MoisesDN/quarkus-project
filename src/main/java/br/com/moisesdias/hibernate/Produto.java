ca5939eb29c8a193923acffe828e54c8883fdaab
Moises Dias <m.dias009@gmail.com>
2026-01-26 08:15:38 -0300

    Add OpenTelemetry configuration and update Produto entity

--- src/main/java/br/com/moisesdias/hibernate/Produto.java
+++ src/main/java/br/com/moisesdias/hibernate/Produto.java
@@ -3,11 +3,14 @@ package br.com.moisesdias.hibernate;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Produto extends PanacheEntityBase {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
 
