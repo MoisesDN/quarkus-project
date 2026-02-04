c71dd3604b5d7a22f209dc9a250fa0a924e3e972
Moises Dias <m.dias009@gmail.com>
2026-02-04 15:15:06 -0300

    Update Keycloak and CORS configuration; simplify ComputadoresResource class

--- src/main/java/br/com/moisesdias/keycloak/ComputadoresResource.java
+++ src/main/java/br/com/moisesdias/keycloak/ComputadoresResource.java
@@ -5,28 +5,20 @@ import jakarta.annotation.security.RolesAllowed;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.container.ContainerRequestContext;
-import jakarta.ws.rs.container.ContainerRequestFilter;
-import jakarta.ws.rs.container.ContainerResponseContext;
-import jakarta.ws.rs.container.ContainerResponseFilter;
 import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.ext.Provider;
 import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
 import org.eclipse.microprofile.openapi.annotations.security.OAuthFlow;
 import org.eclipse.microprofile.openapi.annotations.security.OAuthFlows;
 import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
 import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
 
-import java.io.IOException;
-
 @Path("/computadores")
-@SecurityScheme(securitySchemeName = "quarkus-oauth",
+@SecurityScheme(securitySchemeName = "backend-service",
         type = SecuritySchemeType.OAUTH2,
         flows = @OAuthFlows(password = @OAuthFlow(
                 tokenUrl = "http://localhost:8180/realms/quarkus/protocol/openid-connect/token"
        )))
-@Provider
-public class ComputadoresResource implements ContainerRequestFilter, ContainerResponseFilter {
+public class ComputadoresResource {
 
     @GET
     @Path("publico")
@@ -39,22 +31,8 @@ public class ComputadoresResource implements ContainerRequestFilter, ContainerRe
     @Path("protegido")
     @Produces(MediaType.TEXT_PLAIN)
     @RolesAllowed("user")
-    @SecurityRequirement(name = "quarkus-oauth")
-    public String protegido(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
-        responseContext.getHeaders().putSingle("Access-Control-Allow-Origin", "*");
-        responseContext.getHeaders().putSingle("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-        responseContext.getHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-        responseContext.getHeaders().putSingle("Access-Control-Allow-Credentials", "true");
+    @SecurityRequirement(name = "backend-service")
+    public String protegido() {
         return "Hello, protegido!";
-    }
-
-    @Override
-    public void filter(ContainerRequestContext containerRequestContext) throws IOException {
-
-    }
-
-    @Override
-    public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) throws IOException {
-
     }
 }
